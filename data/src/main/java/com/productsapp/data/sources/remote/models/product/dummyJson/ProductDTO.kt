74cec5c333158ca13caddf0bad32932c382da0c2
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-07-08 23:02:11 +0200

    Fixed Product price data format> int to double

--- data/src/main/java/com/productsapp/data/sources/remote/models/product/dummyJson/ProductDTO.kt
+++ data/src/main/java/com/productsapp/data/sources/remote/models/product/dummyJson/ProductDTO.kt
@@ -7,7 +7,7 @@ data class ProductDTO(
     val discountPercentage: Double = 0.0,
     val id: Int = 0,
     val images: List<String> = listOf(),
-    val price: Int = 0,
+    val price: Double = 0.0,
     val rating: Double = 0.0,
     val stock: Int = 0,
     val thumbnail: String = "",
